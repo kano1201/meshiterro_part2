72b61562f2d8ff2fb2a830c65b68052dd909521e
kano1201 <kano.nakamura3@gmail.com>
2021-06-27 12:36:27 +0000

    画像投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ff0d32ab1804f85aa6c4f1e4b2fc23d8eb27f00a073bf749d262cc443889e3f777bb658e64ba6ade19651032b4508ee579a446843b2cc4ca1438e232c4790919'

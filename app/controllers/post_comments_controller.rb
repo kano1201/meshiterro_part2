e0804d2b938423cc8717ba8c5e113f2badc5903e
kano1201 <kano.nakamura3@gmail.com>
2021-06-29 03:13:26 +0000

    削除ボタン

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -8,6 +8,8 @@ class PostCommentsController < ApplicationController
   end
 
   def destroy
+    PostComment.find_by(id: params[:id], post_image_id: params[:post_image_id]).destroy
+    redirect_to post_image_path(:post_image_id)
   end
 
   private
